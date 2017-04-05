f592f2e81d179c5edeeaabe86bb11a460546a442
Jeff-PC\Jeff <jeffrey@brewstergraphics.com>
2017-04-05 16:36:23 -0700

    Grunt node modules legacy

---
+++ drills_test.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+	<!-- the following 3 tags are for bootstrap -- !-->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Diamond Tools | PCD Drills &amp; Panel Drills by World Diamond Tool</title>
+<meta name="description" content="Manufacturer of PCD Drills and Panel Diamond Drills. Industrial diamond <Dtools by World Diamond Tool" />
+<meta name="keywords" content="pcd drills, panel drills, panel diamond drills, pcd diamond drills, industrial diamond drills" />
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<link href="styles/menu_h2.css" rel="stylesheet" type="text/css" />
+
+<style type="text/css">
+<!--
+.style1 {
+	color: #FF0000;
+	font-weight: bold;
+}
+-->
+</style>
+</head>
+    <body>    
+    <table width="760" border="0" align="center" cellpadding="00" cellspacing="00" bgcolor="#FFFFFF">  
+      <tr>
+      </tr>
+          <td>
+            <?php include '_header.html' ?>
+          </td>
+      </tr>
+      <tr>
+        <td height="42" colspan="3">      
+        <div id="menu_box">
+  			<?php include 'menu.html' ?>    
+    	</div>   
+        </tr>    
+        <tr>   
+          <td height="12" colspan="3"><h2 align="center">PCD Drills &amp; Panel Drills</h2>    
+          </tr>    
+        <tr>   
+        </tr>    
+        <tr>    
+          <td height="12" colspan="3">    <div align="center">    
+            <table width="100%"  border="0" cellspacing="00" cellpadding="4">    
+              <tr>    
+                <td>
+                    <div align="center"><img src="drills_files/image002.jpg" alt="PCD Drills by World Diamond Tool" width="284" height="259" border=				"0" /><img border="0" width="303" height="319" src="drills_files/image004.jpg" />
+                    </div>
+                </td>    
+              </tr>    
+              <tr>    
+                <td class="h3"><table width="100%"  border="0" cellspacing="00" cellpadding="8">    
+              <tr>    
+                 <td><span class="style1">PCD Drills </span>excel in non-ferrous materials as well composite materials. All sizes available    
+    on solid carbide shanks or M-42 cobalt shanks. Please specify your PCD drill: Machine (stubby)    
+    or Jobber length.
+    			</td>    
+                  </tr>    
+                </table>
+                </td>    
+              </tr>    
+              <tr>    
+                <td>
+                	<div align="center"><img src="drills_files/image006.jpg" alt="Panel drill diamond coated" width="497" height="283" border="0" /></div>
+                </td>    
+              </tr>    
+              <tr>    
+                <td class="h3">
+                	<table width="100%"  border="0" cellspacing="00" cellpadding="8">    
+                  <tr>    
+                    <td><span class="style1">Panel Drills</span> can have a replaceable center cutting pilot. These can be a PCD Drill or coated diamond ball mill as shown. This type of drill makes perfect burr free holes in composite panels as well as out lasting carbide by three to one. Panel drills can also be made for flat bottom counter bores. The PCD tips are ground to a brad point.</td>    
+                  </tr>    
+                </table>
+                </td>    
+              </tr>    
+            </table>    
+            </div>    
+        </tr>   
+        <tr>    
+          <td height="6" colspan="3"><div align="center">
+          	<p class="h1_bold">How can World Diamond Tool help buy PCD Drills and Panel Drills? Call <b>714-993-2510</b><b> </b> <strong> </strong></p>   
+            </div>
+         	</td>    
+        </tr>    
+        <tr>    
+          <td height="7" colspan="3"><div align="center"><span class="h1_bold"></span></div></td>    
+        </tr>    
+        <tr>
+        	<td>
+            	<?php include '_footer.html' ?>
+            </td>
+        </tr>  
+    </table>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+   <script src="js/bootstrap.min.js"></script> 
+    </body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
+  <meta name="verify-v1" content="og52bVOaSy8MhXXx3Sgi5i5c/pEkFfIxR9QjnWECjHU=" />
+  <title>Diamond Cutting Tools, Natural Diamond Tools, Diamond-PCB and CBN Tools</title>
+  <meta name="description" content="Manufacturer diamond cutting tools, carbide tools, boron tools, PCD, CBN tools, 
+  inserts including countless industrial natural diamond products by World Diamond Tools">
+  <meta name="keywords" content="diamond tools, industrial diamond tools, carbide tools, boron tools, diamond cutting tools, llllllllPCD CBN"/>
+    <!--[if IE]>
+    <style type="text/css" media="screen">
+     #menu ul li {float: left; width: 100%;}
+    </style>
+    <![endif]-->
+    <!--[if lt IE 7]>
+    <style type="text/css" media="screen">
+    body { 
+    behavior: url(csshover.htc);
+    font-size: 100%;
+    }
+    #menu ul li a {height: 1%;}
+    #menu a, #menu h2 {
+    font: bold 0.7em/1.4em arial, helvetica, sans-serif;
+    }
+    </style>
+    <![endif]-->
+    <link href="styles/menu_h2.css" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+    <div class="content clearfix">
+      <div class="center_box clearfix">
+        <div align="center" class="logo_box">
+        </div>
+
+<!-- MENU INCLUDE -->
+<?php include '_menu.html' ?>
+
+  <div class="copy_box1">
+      <h3>DIAMOND CUTTING TOOLS - CARBIDE TOOLS - BORON TOOLS<br />
+        Standards- Specials- Custom</h3>
+        <p><strong>World Diamond Tool</strong> offers a Complete Line of Diamond Cutting tools.<br />
+          <br/>Check here for <a href="specials.html">Specials</a>.
+        </p>
+        <p>Industrial Diamond Tools &bull; <a href="endmills.html">PCD End Mills</a> &bull; <a href="drills.html">Panel Diamond Drills</a>&bull; <a href="inserts.html">CBN &amp; PCD Inserts</a> &bull;Discs<br /> 
+          &bull; <a href="core_drills.html">Core PCD Drills</a>&bull; <a href="commutators.html">Commutators</a>
+          &bull; <a href="diamond_wheels.html">Diamond Grinding Wheels</a> &bull; Belts &bull; <a href="compounds.html">Diamond Compounds</a> <br>
+          &bull; Carbide Tools &bull; <a href="carbide_routers.html">Carbide Routers</a> &bull; Boron Tools &bull; Plated Tools &nbsp;&bull; <a href="mandrels.html">CBN &amp; Diamond Mandrels</a> <br>&bull; <a href="mandrels_lg.html">Large Mandrels</a> &bull; <a href="files.html">Diamond Files</a> &bull; <a href="countersinks.html">PCD &amp; Plated Countersinks</a> &bull; <a href="groovers.html">PCD &amp; CBN Groovers</a> <br />
+          &bull; <a href="phonopoints.html">Phono Points</a> &bull; <a href="tapered_pins.html">Industrial Pins</a> &bull; <a href="turning_tools.html">PCD &amp; CBN Turning Tools</a> and more. </p>
+          <p align="center">
+            <strong>Diamond Cutting Tools: Solutions for the Most Challenging Applications</strong>
+          </p>
+          <p>
+            World Diamond Tool is dedicated to quality and service.
+          </p>
+          <p>
+            We manufacture and inspect all of our products with precision equipment.  This ensures the highest quality diamond tools. We realize at World Diamond Tool that quick turn-around/service is very important for our customers to achieve their lead times. This comes along with fantastic pricing.
+          </p>
+          <p>
+            Our manfuacturing facility provides a high degree of flexibility for both large and small volume diamond tools.  Our tools have been used in industrial production equipment for many industries including Aerospace, Automotive, Heavy Machinery, Marine, Composites, Reinforced Plastics, Electronics, Military and Fastener Industries.
+          </p>
+          <p align="center">
+            <b>Please contact us for your next diamond cutting tool quote or order and you'll experience our fast and friendly customer service.</b>
+            </p>
+          <div class="specials_box clearfix center_box">
+            <a href="specials.html"><img src="images/cnmg_special.jpg" border="0" alt="Click Here-Diamond Cutting Tools & Specials"/></a>    
+          </div>    
+         </div>
+<!-- FOOTER INCLUDE -->
+<?php include "_footer.html" ?>
+  </body>
+</html>
